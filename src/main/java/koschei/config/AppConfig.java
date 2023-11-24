2418771b7bf9cbd7c1fbad5db7d88de70d1e8528
ignatiev-free <ignatiev.free@yandex.ru>
2023-11-24 21:11:55 +0300

    1 commit

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,6 +1,8 @@
 package koschei.config;
 
+import koschei.models.Egg6;
 import koschei.models.Island2;
+import koschei.models.Needle7;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,7 +13,14 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     @Bean
-    public static Island2 getIsland(Wood3 wood) {
+    public Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
+
+    @Bean
+    public Egg6 getEgg() {
+        return new Egg6();
+    }
 }
+
+
--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,18 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
 
+    @Autowired
+    @Lazy
+    private Egg6 egg;
+
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg.toString();
     }
 }
